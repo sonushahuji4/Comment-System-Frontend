ff23d5bf1447e9540becdaf8443a23f613e3b1e1
Sonu Shahuji <sonushahuji4@gmail.com>
2023-02-11 13:18:16 +0530

    added footer to the application

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,7 +1,17 @@
 const Footer = () => {
     return(
         <div className="footer-container">
-            {"Footer"}
+            <div className="footer-text">
+                <div className="copy-right">
+                    {"© Copyright 2016 - 2023 Flexiple"}
+                </div>
+                <div className="privacy-policy">
+                    {"Privacy Policy"}
+                </div>
+                <div className="terms-of-service">
+                    {"Terms of Service"}
+                </div>
+            </div>
         </div>
     )
 }
